d6204c0d93383ea5d0c4e09d2372e04476f638c1
Anderson Junior <juninho16carvalho@gmail.com>
2024-08-23 20:21:19 -0300

    '

--- aula-23_08/ex1.c
+++ aula-23_08/ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int n1 = 5, n2 = 4;
+
+    // AND
+    int resultado =n1 && n2;
+    printf("%d \n", resultado);
+
+    // OR
+    resultado = n1 || n2;
+    printf("%d \n", resultado);
+
+    // NOT
+    resultado = !(n1 && n2);
+    printf("%d \n", resultado);
+
+    return 0;
+}
