d3e1a4f28e3cf2003a5590376a0a8c29282a7111
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-09-10 12:50:45 -0300

    '

---
+++ vetor/ex10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, tam, soma = 0, vet[8], vetsoma[8];
+
+    for (i=0; i < 8; i++){
+        printf("Digite o elemento %d: \n", i + 1);
+        scanf("%d", &vet[i]);
+        }
+    vetsoma[0] = vet[0];
+    for (i=0; i < 8; i++){
+        vetsoma[i] = vetsoma[i -1] + vet[i];
+    }
+    printf("Soma cumulativa: ");
+    for (i=0; i < 8; i++){
+        printf("%d ", vetsoma[i] );
+    }
+
+    return 0;
+}
