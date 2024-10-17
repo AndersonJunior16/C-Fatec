b8fe3f56d794474258e4bcaa4744c89721ce2dd2
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-10-17 17:23:45 -0300

    Add files via upload

---
+++ matriz/ex6.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, result = 0,
+    mat[3][2], mat_dois[3][3];
+
+    puts("Primeira matriz: ");
+    for (i=0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("Digite o elemento da linha %d coluna %d: \n", i + 1, j+1);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+    puts("Segunda matriz:") ;   
+    for (i=0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("Digite o elemento da linha %d coluna %d: \n", i + 1, j+1);
+            scanf("%d", &mat_dois[i][j]);
+        }
+    }
+    for (i=0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(mat[i][j] == mat_dois[i][j]){
+                result = 0;
+            }
+            else{
+                result = 1;
+            }
+        }
+        
+    }
+    if (result == 0) {
+        printf("As matrizes são idênticas");
+    }
+    else{
+        printf("As matrizes não são idênticas");
+    }
+    return 0;
+}
