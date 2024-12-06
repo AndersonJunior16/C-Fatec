d29354698527be36b52e519d8c93e156c5451023
Anderson Junior <136769269+AndersonJunior16@users.noreply.github.com>
2024-12-06 00:13:59 -0300

    '

---
+++ Arquivos/ex1.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define MAX 2
+
+typedef struct{
+    char nome[50];
+    int idade;
+    float nota;
+}Aluno;
+
+void cadastrar_aluno(Aluno aluno[]){
+    for (int i = 0; i < MAX; i++){
+        printf("%dº - Aluno", i + 1);
+
+        printf("\nDigite o nome do aluno: ");
+        fgets(aluno[i].nome, 50, stdin);
+        setbuf(stdin, NULL);
+        aluno[i].nome[strcspn(aluno[i].nome, "\n")] = 0;
+
+        printf("\nDigite a idade do aluno: ");
+        scanf("%d", &aluno[i].idade);
+        setbuf(stdin, NULL);
+
+        printf("\nDigite a nota do aluno: ");
+        scanf("%f", &aluno[i].nota);
+        setbuf(stdin, NULL);
+        system("cls");
+    }
+}
+
+void mostrar_alunos(Aluno aluno[]){
+    for (int i = 0; i < MAX; i++){
+        printf("\n-=-=-=-%dº Aluno-=-=-=-\n", i + 1);
+        printf("Nome: %s\n", aluno[i].nome);
+        printf("Idade: %d\n", aluno[i].idade);
+        printf("Nota: %.2f\n", aluno[i].nota);
+
+    }
+}
+
+void maior_numero(Aluno aluno[]){
+    int indice_maior = 0;
+
+    for(int i = 0; i < MAX; i++){
+        if (aluno[i].nota > indice_maior){
+            indice_maior = i;
+        }
+    }
+
+    printf("\n-=-=-=-Aluno com a maior nota-=-=-=-\n");
+    printf("Nome: %s\n", aluno[indice_maior].nome);
+    printf("Idade: %d\n", aluno[indice_maior].idade);
+    printf("Nota: %.2f\n", aluno[indice_maior].nota);
+
+}
+
+
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    Aluno aluno[MAX];
+    int opcao  = -1;
+
+    do{
+        printf("\n------Sistema de alunos------\n");
+        printf("1 - Cadastrar alunos\n");
+        printf("2 - Mostrar alunos\n");
+        printf("3 - Mostrar aluno de maior nota\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+        setbuf(stdin, NULL);
+
+        switch(opcao){
+            case 1:
+                system("cls");
+                cadastrar_aluno(aluno);
+                break;
+            case 2:
+                system("cls");
+                mostrar_alunos(aluno);          
+                break;
+            case 3:
+                system("cls");
+                maior_numero(aluno);
+                break;
+            case 0:
+                system("cls");
+                printf("Saindo...");
+                break;  
+            default:
+                system("cls");
+                printf("Opção invalida");             
+            }
+    }while (opcao != 0);
+    return 0;
+}
